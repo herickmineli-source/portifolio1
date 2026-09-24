23b6c8e18c1473babe59f111dddf9f40b42ff362
herickmineli-source <herick.mineli@escola.pr.gov.br>
2026-09-24 09:39:29 -0300

    Add files via upload

--- atividade 3/script.js
+++ atividade 3/script.js
@@ -6,7 +6,12 @@ function calcular (){
  nota1trim = Number(prompt("digite a nota do primeiro trimestre:"));
 nota2trim = Number(prompt("digite a nota do segundo trimestres:"));
 
-resultado = 180 - nota1trim + nota2trim;
+resultado = 180 - (nota1trim + nota2trim);
 
+if (resultado <= 0){
+alert("Parabéns! Você está aprovado!");
+} else {
+alert("Você ainda precisa tirar " + resultado);
+}
 
 }
